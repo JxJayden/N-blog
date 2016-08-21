f7fc4b56fd083731c501661c6bf9e97aa96904b8
jayden <jxdjayden@gmail.com>
2016-08-22 00:52:25 +0800

    建立项目框架

---
+++ blog/routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* 主页 */
+router.get('/', function(req, res) {
+  res.render('index', { title: '主页' });
+});
+
+/* 注册 */
+router.get('/reg', function(req, res) {
+  res.render('index', { title: '注册' });
+});
+
+/* 登录 */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: '登录' });
+});
+
+/* 退出 */
+router.get('/logout', function(req, res, next) {
+});
+
+router.post('/reg', function (req, res) {
+});
+
+router.post('/login', function (req, res) {
+});
+
+router.post('/post', function (req, res) {
+});
+
+module.exports = router;
