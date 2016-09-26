8711e30d1c290f8ee386aea53f7fbedc4f57bfd2
jayden <jxdjayden@gmail.com>
2016-09-27 00:31:22 +0800

    更新 编辑页面和留言区的markdown

--- blog/public/javascripts/common.js
+++ blog/public/javascripts/common.js
@@ -1,6 +1,16 @@
-var SimpleMDE = require('simplemde');
-
-var simplemde = new SimpleMDE({
- element: document.getElementById("editor"),
- initialValue: '请愉快地使用 markdown 写作吧！～'
-});
+var element = document.getElementById('edit_editor') || document.getElementById("post_editor") || document.getElementById('comment_editor'),
+    simplemde = new SimpleMDE({
+        element: element,
+        autofocus: element.id === 'edit_editor' ? true : false,
+        placeholder: '请愉快地使用 markdown 写作吧！～',
+        renderingConfig: {
+            singleLineBreaks: false,
+            codeSyntaxHighlighting: true,
+        },
+        forceSync: true,
+        spellChecker: false,
+        styleSelectedText: false,
+        toolbar: element.id === 'comment_editor' ? false : true,
+        status: element.id === 'comment_editor' ? false : true,
+        showIcons: ["code"]
+    });
