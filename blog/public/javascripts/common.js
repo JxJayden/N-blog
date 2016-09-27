2d6e1e9e2362bd02fd749f7c2571cfe2218cff2c
jayden <jxdjayden@gmail.com>
2016-09-27 22:25:00 +0800

    update

--- blog/public/javascripts/common.js
+++ blog/public/javascripts/common.js
@@ -1,8 +1,8 @@
 var element = document.getElementById('edit_editor') || document.getElementById("post_editor") || document.getElementById('comment_editor'),
-    simplemde = new SimpleMDE({
+    option = {
         element: element,
         autofocus: element.id === 'edit_editor' ? true : false,
-        placeholder: '请愉快地使用 markdown 写作吧！～',
+        placeholder: element.id === 'comment_editor'?'留言区～这里也可以用 markdown 语法哦！':'请愉快地使用 markdown 写作吧！～',
         renderingConfig: {
             singleLineBreaks: false,
             codeSyntaxHighlighting: true,
@@ -11,4 +11,10 @@ var element = document.getElementById('edit_editor') || document.getElementById(
         spellChecker: false,
         styleSelectedText: false,
         showIcons: ["code"]
-    });
+    };
+
+if (element.id === 'comment_editor') {
+ option.toolbar = false;
+}
+
+var simplemde = new SimpleMDE(option);
